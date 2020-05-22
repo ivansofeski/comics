0a6a6624f3f9d2e8344369cccb1b0a20f5065dda
Ivan Sofeski <ivan.sofeski@ingka.com>
2020-05-22 22:49:10 +0200

    Updates

---
+++ precache-manifest.1d7f5df3019936c6d548677844c154e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a29e25994856a82c2eea151d97807d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18da1aeb3f0fac391075",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "12e2756629d4a4c997bf",
+    "url": "/static/js/2.41d9165f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41d9165f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18da1aeb3f0fac391075",
+    "url": "/static/js/main.5f8cbf5a.chunk.js"
+  },
+  {
+    "revision": "5493ad76b908356b4e23",
+    "url": "/static/js/runtime-main.b59dd88d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
